00bbfc39a137bfa229392fdcbce4010f092a9a30
JaviFroy <60381987+JaviFroy@users.noreply.github.com>
2021-01-09 12:33:55 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -6,17 +6,19 @@ const mongoose = require('mongoose');
 const port = 3000;
 const jwt = require('jsonwebtoken');
 const config = {
-	llave : "miclaveultrasecreta123*"
+  llave: "miclaveultrasecreta123*"
 };
 
 app.use(cors());
 app.set('llave', config.llave);
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
 app.options(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 //MONGO ATLAS
@@ -24,89 +26,109 @@ const db = "assets";
 var user = "client";
 var password = "client";
 const mongoString = "mongodb+srv://" + user + ":" + password + "@realmcluster.uidt7.mongodb.net/" + db + "?retryWrites=true&w=majority"
-mongoose.connect(mongoString, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 mongoose.connection.on("error", function (error) {
-    console.log(error)
+  console.log(error)
 })
 mongoose.connection.on("open", function () {
-    console.log("Connected to MongoDB database " + db)
+  console.log("Connected to MongoDB database " + db)
 })
 
-const { Schema } = mongoose;
+const {
+  Schema
+} = mongoose;
 const elementSchema = new Schema({
-    name: String,
-    icon: String,
-    color: String,
-    description: String
-}, { strict: false }, { versionKey: false });
+  name: String,
+  icon: String,
+  color: String,
+  description: String
+}, {
+  strict: false
+}, {
+  versionKey: false
+});
 const Element = mongoose.model('elements', elementSchema);
 
 const characterSchema = new Schema({
-    id: String,
-    name: String,
-    image: String,
-    mode: String,
-    age: Number,
-    mom_race: Object,
-    dad_race: Object,
-    first_element: Object,
-    second_element: Object,
-    equip: Object,
-    attributes: Object,
-    description: String
-}, { strict: false }, { versionKey: false });
+  id: String,
+  name: String,
+  image: String,
+  mode: String,
+  age: Number,
+  mom_race: Object,
+  dad_race: Object,
+  first_element: Object,
+  second_element: Object,
+  equip: Object,
+  attributes: Object,
+  description: String
+}, {
+  strict: false
+}, {
+  versionKey: false
+});
 const Character = mongoose.model('characters', characterSchema, 'characters');
 
 // GET INI
 app.get('/', function (req, res, next) {
-    var pm = "WELCOMO TO LEGENDS OF ASKARON"
-    console.log(pm);
-    res.send(pm);
+  var pm = "WELCOMO TO LEGENDS OF ASKARON"
+  console.log(pm);
+  res.send(pm);
 })
 
 
 app.post('/login', function (req, res, next) {
-    console.log(req.body,req.body.username,req.body.password);
-    if (!req.body) {
-        return res.status(400).json({
-            status: 'error',
-            error: 'req body cannot be empty',
-        });
+  console.log(req.body, req.body.username, req.body.password);
+  if (!req.body) {
+    return res.status(400).json({
+      status: 'error',
+      error: 'req body cannot be empty',
+    });
+  } else {
+    if (req.body.username == "javi") {
+      console.log("login");
+      const payload = {
+        check: true
+      };
+      const token = jwt.sign(payload, app.get('llave'), {
+        expiresIn: 1440
+      });
+      res.json({
+        token: token
+      });
     } else {
-        if (req.body.username == "javi") {
-            console.log("login");
-            const payload = {check: true};
-            const token = jwt.sign(payload, app.get('llave'), {expiresIn: 1440});
-            res.json({
-                mensaje: 'Autenticación correcta',
-                token: token
-            });
-        } else {
-            console.log("not login");
-            res.json({mensaje:"user: " +req.body.name });
-        }
+      console.log("not login");
+      res.json({
+        mensaje: "user: " + req.body.name
+      });
     }
+  }
 });
 
 
 const rutasProtegidas = express.Router();
 rutasProtegidas.use((req, res, next) => {
-    const token = req.headers['access-token'];
+  const token = req.headers['access-token'];
 
-    if (token) {
-        jwt.verify(token, app.get('llave'), (err, decoded) => {
-            if (err) {
-                return res.json({ mensaje: 'Token inválida' });
-            } else {
-                req.decoded = decoded;
-                next();
-            }
+  if (token) {
+    jwt.verify(token, app.get('llave'), (err, decoded) => {
+      if (err) {
+        return res.json({
+          mensaje: 'Token inválida'
         });
-    } else {
-        res.send({
-            mensaje: 'Token no proveída.'
-        });
-    }
+      } else {
+        req.decoded = decoded;
+        next();
+      }
+    });
+  } else {
+    res.send({
+      mensaje: 'Token no proveída.'
+    });
+  }
 });
 
 
@@ -115,99 +137,111 @@ rutasProtegidas.use((req, res, next) => {
 
 // GET ALL CHARACTERS
 app.get('/characters', rutasProtegidas, function (req, res, next) {
-    const query = Character.find();
-    query.sort('_id');
-    query.select("-_id -__v");
-    // execute the query at a later time
-    query.exec(function (err, characters) {
-        if (err) return handleError(err);
-        console.log(characters)
-        res.send(characters);
-    });
+  const query = Character.find();
+  query.sort('_id');
+  query.select("-_id -__v");
+  // execute the query at a later time
+  query.exec(function (err, characters) {
+    if (err) return handleError(err);
+    console.log(characters)
+    res.send(characters);
+  });
 })
 
 // GET CHARACTER BY ID
 app.get('/characters/:id', function (req, res, next) {
-    var id = req.params.id;
-    const query = Character.findOne({ "id": id });
-    query.select("-_id -__v");
-    // execute the query at a later time
-    query.exec(function (err, character) {
-        if (err) return handleError(err);
-        console.log(character)
-        res.send(character);
-    });
+  var id = req.params.id;
+  const query = Character.findOne({
+    "id": id
+  });
+  query.select("-_id -__v");
+  // execute the query at a later time
+  query.exec(function (err, character) {
+    if (err) return handleError(err);
+    console.log(character)
+    res.send(character);
+  });
 });
 // POST NEW CHARACTER
 app.post('/characters', function (req, res, next) {
-    if (!req.body) {
-        return res.status(400).json({
-            status: 'error',
-            error: 'req body cannot be empty',
-        });
-    } else {
-        res.status(200).json({
-            status: 'succes',
-            data: req.body,
-        });
+  if (!req.body) {
+    return res.status(400).json({
+      status: 'error',
+      error: 'req body cannot be empty',
+    });
+  } else {
+    res.status(200).json({
+      status: 'succes',
+      data: req.body,
+    });
 
-        var character = {
-            "name": req.body.name,
-            "age": req.body.age,
-            "first_element": { "name": req.body.first_element },
-            "second_element": { "name": req.body.second_element }
-        }
-        Character.watch()
-        Character.create(character, function (err, res) {
-
-            if (err) return handleError(err);
-        });
-
-
+    var character = {
+      "name": req.body.name,
+      "age": req.body.age,
+      "first_element": {
+        "name": req.body.first_element
+      },
+      "second_element": {
+        "name": req.body.second_element
+      }
     }
+    Character.watch()
+    Character.create(character, function (err, res) {
+
+      if (err) return handleError(err);
+    });
+
+
+  }
 });
 
 // UPDATE CHARACTER BY ID
 app.put('/characters/:id', function (req, res, next) {
-    if (!req.body) {
-        return res.status(400).json({
-            status: 'error',
-            error: 'req body cannot be empty',
-        });
-    } else {
-        res.status(200).json({
-            status: 'succes',
-            data: req.body,
-        });
-        var id = req.params.id;
-        const query = Character.findOneAndUpdate({ "id": id }, { "name": req.body.name });
-        query.exec();
-    }
+  if (!req.body) {
+    return res.status(400).json({
+      status: 'error',
+      error: 'req body cannot be empty',
+    });
+  } else {
+    res.status(200).json({
+      status: 'succes',
+      data: req.body,
+    });
+    var id = req.params.id;
+    const query = Character.findOneAndUpdate({
+      "id": id
+    }, {
+      "name": req.body.name
+    });
+    query.exec();
+  }
 })
 
 // DELETE CHARACTER BY ID
 app.delete('/characters/:id', function (req, res, next) {
-    var id = req.params.id;
-    const query = Character.findOneAndDelete({ "id": id });
-    query.select("-_id -__v");
-    // execute the query at a later time
-    query.exec(function (err, character) {
-        if (err) return handleError(err);
-        console.log(character)
-        res.send(character);
-    });
+  var id = req.params.id;
+  const query = Character.findOneAndDelete({
+    "id": id
+  });
+  query.select("-_id -__v");
+  // execute the query at a later time
+  query.exec(function (err, character) {
+    if (err) return handleError(err);
+    console.log(character)
+    res.send(character);
+  });
 });
 
 // GET ALL ELEMENTS
 app.get('/elements', function (req, res, next) {
-    const query = Element.find();
-    query.sort("_id")
-    query.select("-_id");
-    // execute the query at a later time
-    query.exec(function (err, elements) {
-        if (err) return handleError(err);
-        res.send(elements);
-    });
+  const query = Element.find();
+  query.sort("_id")
+  query.select("-_id");
+  // execute the query at a later time
+  query.exec(function (err, elements) {
+    if (err) return handleError(err);
+    res.send(elements);
+  });
 });
 
 app.listen(process.env.PORT || port);
